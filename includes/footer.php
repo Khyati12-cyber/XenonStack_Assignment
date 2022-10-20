4602ca48a21646d4fececa880ca2fbbdf415244a
khyati gupta <81294185+Khyati12-cyber@users.noreply.github.com>
2022-10-20 11:31:53 +0530

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,4 +1,4 @@
 <footer class="footer">
-    <div class="container text-center"><span class="text-muted"><b>Treasury Store  | Contact Us: +91 90000 00000</b></span></div>
+    <div class="container text-center"><span class="text-muted"><b>Treasury Store  |Email us-Khyati020799@gmail.com | Contact Us: +91 90000 00000</b></span></div>
 </footer>
     
