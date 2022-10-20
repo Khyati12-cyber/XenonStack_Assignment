1bf2f5d8ab705a76cb9358fa5191279358f718d8
khyati gupta <81294185+Khyati12-cyber@users.noreply.github.com>
2022-10-20 11:11:18 +0530

    Update ecommerce.sql

--- ecommerce.sql
+++ ecommerce.sql
@@ -1,32 +1,12 @@
--- phpMyAdmin SQL Dump
--- version 4.8.2
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Mar 25, 2019 at 05:04 PM
--- Server version: 10.1.34-MariaDB
--- PHP Version: 7.2.7
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
 -- Database: `ecommerce`
---
 
--- --------------------------------------------------------
-
---
--- Table structure for table `products`
---
 
 CREATE TABLE `products` (
   `id` int(11) NOT NULL,
@@ -34,9 +14,6 @@ CREATE TABLE `products` (
   `price` int(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `products`
---
 
 INSERT INTO `products` (`id`, `name`, `price`) VALUES
 (1, 'Guess 1875', 3000),
@@ -56,11 +33,7 @@ INSERT INTO `products` (`id`, `name`, `price`) VALUES
 (15, 'Sony Speaker', 10500),
 (16, 'Airpods', 15000);
 
--- --------------------------------------------------------
 
---
--- Table structure for table `users`
---
 
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
@@ -72,9 +45,6 @@ CREATE TABLE `users` (
   `password` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `users`
---
 
 INSERT INTO `users` (`id`, `email`, `name`, `contact`,'city','address', `password`) VALUES
 (65, 'sharew5m123@gmail.com', 'reys', 789425631,'jhansi','kharar', 'Kg12345678@');
